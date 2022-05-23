74b545552b8b23a2f3dc90b3c3a248692039e479
graciabound <nneosoka@gmail.com>
2022-05-23 06:22:12 +0000

    amended monitor file

---
+++ monitor.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+echo welcome to landmarktech
+echo how many we help you
+echo to recharge your phone,press 1
+read $1
+echo to speak with cso, press 2
+read $2
+echo thaank you for stopping by
+
